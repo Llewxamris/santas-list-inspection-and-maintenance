f98d2aab3bf252112024883268228318f5f31943
Maxwell Haley <maxwell.haley@openmailbox.org>
2016-12-07 22:43:53 -0500

    Various changes Scripts now parses the JSON string to a JSON object. Other changes have mostly no purpose other than testing.

--- node/public/javascript/scripts.js
+++ node/public/javascript/scripts.js
@@ -1,20 +1,21 @@
-function displayLists(jsonFile) {
+function displayLists(jsonString) {
+    // console.log(jsonFile['person']['-id']);
+    let json = JSON.parse(jsonString);
     let content = $('#content');
     let html = "";
     content.html('<br />');
-    $.each(jsonFile.person, (index, element) => {
-        // content.append('<div class="person">');
+    $.each(json.person, (index, element) => {
         html += '<div class="person">';
         $.each(element, (index, value) => {
             html += `<p>${index}${value}</p>`;
-            // content.append(`<p>${index}${value}</p>`);
         });
-        // content.append('</div>');
         html += '</div>';
         content.append(html);
+        content.append('<br />');
     });
 
 }
+
 // $(document).on('click', '.dropdown-menu li a', function() {
 //     let listName = $(this).text().trim().substring(0,1);
 //     console.log('lol');
