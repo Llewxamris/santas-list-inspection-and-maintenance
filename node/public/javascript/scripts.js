aef0bcd001102111cfa3a126e9ecd002d8e7981b
Maxwell Haley <maxwell.haley@openmailbox.org>
2016-12-08 13:49:00 -0500

    See commit for mylist.html

--- node/public/javascript/scripts.js
+++ node/public/javascript/scripts.js
@@ -2,18 +2,87 @@ function displayLists(jsonString) {
     // console.log(jsonFile['person']['-id']);
     let json = JSON.parse(jsonString);
     let content = $('#content');
-    let html = "";
+    let toInsert = "";
+    let fName = '';
+    let lname = '';
     content.html('<br />');
     $.each(json.person, (index, element) => {
-        html += '<div class="person">';
+        toInsert = '';
+        fName = '';
+        lName = '';
+        toInsert += '<div value="test" class="person">';
         $.each(element, (index, value) => {
-            html += `<p>${index}${value}</p>`;
+            if(index === '-id') {
+                toInsert += `<h3 class='id'>${value}</h3>`;
+            }
+            if(index === 'firstName') {
+                fName = value;
+            }
+            if(index === 'lastName') {
+                lName = value;
+            }
         });
-        html += '</div>';
-        content.append(html);
+        toInsert += fName + ' ' + lName;
+        toInsert += '</div>';
+        content.append(toInsert);
         content.append('<br />');
     });
 
+}
+
+function displaySpecific(jsonString) {
+    console.log(jsonString);
+    let fuck = '{ "person": [{"-id": "3","firstName": "John","lastName": "Doe","age": "53","city": "London","curList": "L","details": "","dateUpdated": "0404 - 1212 - 2016"}]}';
+    let json     = JSON.parse(fuck);
+    let content  = $('#specific');
+    let toInsert = '';
+    let id;
+    let fName;
+    let lName;
+    let age;
+    let city;
+    let deta;
+    let date;
+
+    content.html('');
+
+    $.each(json.person, (index, element) => {
+        $.each(element, (index, value) => {
+            switch (index) {
+              case '-id':
+                id = value;
+                break;
+              case 'lastName':
+                lName = value;
+                break;
+              case 'firstName':
+                fName = value;
+                break;
+              case 'age':
+                age = value; 
+                break;
+              case 'city':
+                city = value;
+                break;
+              case 'details':
+                deta = value;
+                break;
+              case 'dateUpdated':
+                date = value;
+                break;
+              default:
+                console.log(`Key:Value ${index}:${value} not handled.`);
+            }
+        });
+        toInsert += '<div>';
+        toInsert += 'Name: '         + fName + ' ' + lName + '<br />';
+        toInsert += 'Age: '          + age   + '<br />';
+        toInsert += 'City: '         + city  + '<br />';
+        toInsert += 'Details: '      + deta  + '<br />';
+        toInsert += 'Date Updated: ' + date  + '<br />';
+        toInsert += '</div>';
+        content.append(toInsert);
+    });
 }
 
 // $(document).on('click', '.dropdown-menu li a', function() {
@@ -26,14 +95,22 @@ function displayLists(jsonString) {
 //     });
 // });
 
-$(document).on('click', '.dropdown', function(e) {
+$(document).on('click', '.dropdown', (e) => {
     let listName = e.target.value;
-    $.get('/', { which: listName }, function (data) {
+
+    $.get('/', { which: listName },  (data) => {
         displayLists(data);
     });
 });
 
+$(document).on('click', '.person .id',  (e) => {
+    let personId = $(e.target).text();
 
+    $.get('/', { "id": personId }, (data) => {
+        displaySpecific(data);
+    });
+});
+ 
 $(function () {
     // loadListNames();
     'use strict';
